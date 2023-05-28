2457389b8ff9f03b073b0a9b24d487fafa3afe31
alice <ajsc1@aluno.ifal.edu.brm>
2023-05-28 15:44:52 -0300

    Dicee Challenge

---
+++ index.js
@@ -0,0 +1,22 @@
+let diceP1 = Math.floor(Math.random() * 6) + 1 ;
+
+let randomP1 = "./images/dice" +diceP1+".png";
+
+let diceP2 = Math.floor(Math.random() * 6) + 1;
+
+let randomP2 = "./images/dice" +diceP2+".png";
+
+
+document.querySelector(".img1").setAttribute("src", randomP1)
+
+
+document.querySelector(".img2").setAttribute("src", randomP2)
+
+if(diceP1 > diceP2){
+    document.querySelector("h1").innerText = "Player 1 wins"
+} else if(diceP1 < diceP2){
+    document.querySelector("h1").innerText = "Player 2 wins"
+} else if( diceP1 == diceP2){
+    document.querySelector("h1").innerText = "Draw!"
+
+}
